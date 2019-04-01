5268f0410e6d3997a589ac9950a4511c3378f38c
Andres Gonzalez <agonzalezjr@gmail.com>
2019-04-01 16:10:30 -0400

    Add prod env (== dev)

---
+++ src/config/prod.js
@@ -0,0 +1,8 @@
+export const FirebaseConfig = {
+  apiKey: "AIzaSyDVzb1g3jyBztIojOEpM_MN-Nk77utLh9k",
+  authDomain: "firedux-todo-777a2.firebaseapp.com",
+  databaseURL: "https://firedux-todo-777a2.firebaseio.com",
+  projectId: "firedux-todo-777a2",
+  storageBucket: "firedux-todo-777a2.appspot.com",
+  messagingSenderId: "172026999781"
+}
